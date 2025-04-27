de2cf1e0953e58c2047e831d5671453f2184f277
alemaki <alemak2002@gmail.com>
2025-04-27 11:49:34 +0300

    Added base classes for logger.

--- src/utils/logger/logger.cpp
+++ src/utils/logger/logger.cpp
@@ -1,5 +1,40 @@
 #include "logger.hpp"
 
+bool utils::Logger::is_class_enabled(const godot::StringName &name) const
+{
+    if (enabled_classes.has(name))
+    {
+        return true;
+    }
+    else if (disabled_classes.has(name))
+    {
+        return false;
+    }
+    
+    if (!this->every_class_enabled) /* No need to check both bases */
+    {
+        for (const godot::StringName& base_class : enabled_base_classes)
+        {
+            if(is_subclass(name, base_class))
+            {
+                return true;
+            }
+        }
+    }
+    else
+    {
+        for (const godot::StringName& base_class : disabled_base_classes)
+        {
+            if(is_subclass(name, base_class))
+            {
+                return false;
+            }
+        }
+    }
+
+    return this->every_class_enabled;
+}
+
 void utils::Logger::disable_class(const godot::StringName &name)
 {
     this->disabled_classes.insert(name);
@@ -10,6 +45,19 @@ void utils::Logger::enable_class(const godot::StringName &name)
 {
     this->enabled_classes.insert(name);
     this->disabled_classes.erase(name);
+}
+
+void utils::Logger::disable_base_class(const godot::StringName &name)
+{
+    this->disabled_base_classes.insert(name);
+    this->enabled_base_classes.erase(name);
+}
+
+void utils::Logger::enable_base_class(const godot::StringName &name)
+{
+    
+    this->enabled_base_classes.insert(name);
+    this->disabled_base_classes.erase(name);
 }
 
 void utils::Logger::enable_level(Level level)
--- src/utils/logger/logger.hpp
+++ src/utils/logger/logger.hpp
@@ -31,51 +31,49 @@ namespace utils
 
         godot::HashSet<godot::StringName> enabled_classes = {};
         godot::HashSet<godot::StringName> disabled_classes = {};
+        godot::HashSet<godot::StringName> enabled_base_classes = {};
+        godot::HashSet<godot::StringName> disabled_base_classes = {};
+
 
         CREATE_GETTER_SETTER_BOOL_DEFAULT(every_class_enabled);
 
-        inline bool is_class_enabled(const godot::StringName& name) const
-        {
-            if ((this->every_class_enabled) && (!disabled_classes.has(name)))
-            {
-                return true;
-            }
-            else if ((!this->every_class_enabled) && (enabled_classes.has(name)))
-            {
-                return true;
-            }
-            return false;
-        }
+        bool is_class_enabled(const godot::StringName& name) const;
+
         void disable_class(const godot::StringName& name);
         void enable_class(const godot::StringName& name);
+        void disable_base_class(const godot::StringName& name);
+        void enable_base_class(const godot::StringName& name);
 
         void enable_level(Level level);
         void disable_level(Level level);
         void enable_all_levels();
 
-        template <typename T>
-        inline void log_info(godot::String message)
+        inline void log_info(godot::StringName class_name, godot::String message)
         {
-            if (((this->enabled & Level::INFO) == 0) || (!this->is_class_enabled(T::get_class_static()))) 
+            if (((this->enabled & Level::INFO) == 0) || (!this->is_class_enabled(class_name))) 
             {
                 return;
             }
-            godot::UtilityFunctions::print("[INFO]", "[", T::get_class_static(), "]", message);
+            godot::UtilityFunctions::print("[INFO]", "[", class_name, "]", message);
         }
 
-        template <typename T>
-        inline void log_debug(godot::String message)
+        inline void log_debug(godot::StringName class_name, godot::String message)
         {
-            if ((this->enabled & Level::DEBG) == 0 || (!this->is_class_enabled(T::get_class_static()))) 
+            if ((this->enabled & Level::DEBG) == 0 || (!this->is_class_enabled(class_name))) 
             {
                 return;
             }
-            godot::UtilityFunctions::print("[DEBG]", "[", T::get_class_static(), "]", message);
+            godot::UtilityFunctions::print("[DEBG]", "[", class_name, "]", message);
         }
     };
 
     inline Logger* Logger::instance = new Logger();
 
 } // namespace utils
+
+#define LOG_DEBUG_AS(tag, message) utils::Logger::instance->log_debug(tag, message)
+#define LOG_DEBUG(message) utils::Logger::instance->log_debug(this->get_class(), message)
+#define LOG_INFO_AS(tag, message) utils::Logger::instance->log_info(tag, message)
+#define LOG_INFO(message) utils::Logger::instance->log_info(this->get_class(), message)
 
 #endif /* SRC_LOGGER_HPP */
