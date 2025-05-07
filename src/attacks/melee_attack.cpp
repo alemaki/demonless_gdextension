2b2d398a098ec3fe289cb9a065676fc21c4780d6
alemaki <alemak2002@gmail.com>
2025-05-07 15:06:55 +0300

    Melee attack updated to hold hitbox.

--- src/attacks/melee_attack.cpp
+++ src/attacks/melee_attack.cpp
@@ -3,10 +3,36 @@
 void MeleeAttack::_step(double delta)
 {
     ERR_FAIL_NULL(this->actor_source);
-    ERR_FAIL_COND(direction.is_zero_approx());
-    bool cancellable = this->time_accumulated <= this->early_cancel_endpoint
-                       || this->time_accumulated >= this->late_cancel_startpoint;
+    bool cancellable = (this->time_accumulated <= this->early_cancel_endpoint)
+                       || (this->time_accumulated >= this->late_cancel_startpoint);
     this->set_cancellable(cancellable);
+
+    bool hitbox_should_be_active = (this->time_accumulated >= this->hit_startpoint) 
+                            && (this->time_accumulated <= this->hit_endpoint);
+
+    if (hitbox_should_be_active && !this->is_hitbox_active)
+    {
+        ERR_FAIL_NULL(this->hitbox);
+        ERR_FAIL_COND(direction.is_zero_approx());
+        godot::Vector3 source_position = this->actor_source->get_global_position();
+        this->hitbox->set_monitoring(true);
+        this->hitbox->set_visible(true);
+        this->hitbox->set_global_position(source_position);
+        this->hitbox->look_at(source_position + this->get_direction());
+        this->is_hitbox_active = true;
+    }
+    else if (this->is_hitbox_active)
+    {
+        if (this->change_direction_allowed)
+        {
+            this->hitbox->look_at(this->actor_source->get_global_position() + this->get_direction());
+        }
+        if (!hitbox_should_be_active)
+        {
+            this->hitbox->set_visible(false);
+            this->hitbox->set_monitoring(false);
+        }
+    }
 }
 
 void MeleeAttack::_reset()
@@ -19,10 +45,23 @@ void MeleeAttack::_end()
     this->set_cancellable(false);
 }
 
+void MeleeAttack::_ready()
+{
+    utils::ensure_node(this->hitbox, this, "Hitbox");
+    if (this->hitbox)
+    {
+        this->hitbox->set_monitoring(false);
+        this->hitbox->set_visible(false);
+    }
+}
+
 void MeleeAttack::_bind_methods()
 {
     using namespace godot;
     
     BIND_GETTER_SETTER_PROPERTY_DEFAULT(MeleeAttack, FLOAT, early_cancel_endpoint);
     BIND_GETTER_SETTER_PROPERTY_DEFAULT(MeleeAttack, FLOAT, late_cancel_startpoint);
+    BIND_GETTER_SETTER_PROPERTY_DEFAULT(MeleeAttack, FLOAT, hit_endpoint);
+    BIND_GETTER_SETTER_PROPERTY_DEFAULT(MeleeAttack, FLOAT, hit_startpoint);
+    BIND_GETTER_SETTER_PROPERTY_OBJECT_DEFAULT(MeleeAttack, hitbox, PROPERTY_HINT_NODE_TYPE, "Hitbox", PROPERTY_USAGE_DEFAULT, Hitbox);
 }
--- src/attacks/melee_attack.hpp
+++ src/attacks/melee_attack.hpp
@@ -2,23 +2,37 @@
 #define MELEE_ATTACK_HPP
 
 #include "skills/skill_action.hpp"
+#include "components/area3d/hitbox.hpp"
+#include "utils/utils.hpp"
 
 class MeleeAttack : public SkillAction
 {
     GDCLASS(MeleeAttack, SkillAction)
 
+private:
+    bool is_hitbox_active = false;
+
 protected:
     double early_cancel_endpoint = 0.2;
     double late_cancel_startpoint = 0.8;
-    /* TODO: Hitbox */
+    double hit_startpoint = 0.2;
+    double hit_endpoint = 0.8;
+    Hitbox* hitbox = nullptr;
+    
 public:
     CREATE_GETTER_SETTER_POSITIVE_DEFAULT(double, early_cancel_endpoint);
     CREATE_GETTER_SETTER_POSITIVE_DEFAULT(double, late_cancel_startpoint);
+    CREATE_GETTER_SETTER_POSITIVE_DEFAULT(double, hit_startpoint);
+    CREATE_GETTER_SETTER_POSITIVE_DEFAULT(double, hit_endpoint);
+    CREATE_GETTER_SETTER_DEFAULT(Hitbox*, hitbox);
 
 private:
     void _reset() override;
     void _step(double delta) override;
     void _end() override;
+
+public:
+    void _ready() override;
 
 protected:
     static void _bind_methods();
