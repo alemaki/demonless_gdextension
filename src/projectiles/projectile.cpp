9a7442d7c2f1b40e96f77bd539b50fdef5c22c23
Pamela Slavcheva <pamivladi@gmail.com>
2025-07-15 23:07:06 +0300

    Added lifespan to projectile.

--- src/projectiles/projectile.cpp
+++ src/projectiles/projectile.cpp
@@ -1,9 +1,26 @@
 #include "projectile.hpp"
 
+void Projectile::_ready()
+{
+    if (!this->lifespan_timer)
+    {
+        this->lifespan_timer = memnew(godot::Timer);
+        this->add_child(this->lifespan_timer);
+        this->lifespan_timer->set_autostart(true);
+        this->lifespan_timer->set_one_shot(true);
+    }
+}
 
 void Projectile::_bind_methods()
 {
     using namespace godot;
 
-    /* ClassDB::bind_method(D_METHOD("_physics_process", "delta"), &Projectile::_physics_process); */
+    ClassDB::bind_method(D_METHOD("set_lifespan_timer", "timer"), &Projectile::set_lifespan_timer);
+    ClassDB::bind_method(D_METHOD("get_lifespan_timer"), &Projectile::get_lifespan_timer);
+    ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "lifespan_timer", PropertyHint::PROPERTY_HINT_NODE_TYPE, "Timer",PropertyUsageFlags::PROPERTY_USAGE_DEFAULT, "Timer"), "set_lifespan_timer", "get_lifespan_timer");
+}
+
+void Projectile::set_lifespan_timer(godot::Timer* timer)
+{
+    this->lifespan_timer = timer;
 }
--- src/projectiles/projectile.hpp
+++ src/projectiles/projectile.hpp
@@ -2,13 +2,23 @@
 #define PROJECTILE_HPP
 
 #include <godot_cpp/classes/node3d.hpp>
+#include <godot_cpp/classes/timer.hpp>
 
 class Projectile : public godot::Node3D
 {
     GDCLASS(Projectile, godot::Node3D);
 
 protected:
+    godot::Timer* lifespan_timer = nullptr;
     static void _bind_methods();
+
+public:
+    void _ready() override;
+    void set_lifespan_timer(godot::Timer* timer);
+    _FORCE_INLINE_ godot::Timer* get_lifespan_timer() const
+    {
+        return this->lifespan_timer;
+    }
 };
 
 #endif // PROJECTILE_HPP
