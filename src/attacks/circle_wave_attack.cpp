289a430d277f5ccc2a0c54c23616f5aaa938bf14
Alexander Makedonski <alemak2002@gmail.com>
2025-07-19 14:39:01 +0300

    Added CircleWaveAttack class.

--- src/attacks/circle_wave_attack.cpp
+++ src/attacks/circle_wave_attack.cpp
@@ -1,8 +1,42 @@
 #include "circle_wave_attack.hpp"
 
+void CircleWaveAttack::set_direction(const godot::Vector3 direction)
+{
+    ERR_FAIL_COND_MSG(direction.is_zero_approx(), "Attack direction cannot be godot::Vector3{0, 0, 0}");
+    this->direction = direction;
+}
+
+void CircleWaveAttack::_reset()
+{
+    this->Attack::reset();
+    this-> shots_fired = 0;
+}
+
 void CircleWaveAttack::_step(double delta)
 {
+    ERR_FAIL_NULL(this->actor_source);
+    ERR_FAIL_NULL(this->projectile);
+    ERR_FAIL_COND(direction.is_zero_approx());
 
+    int shots_to_fire = this->shots - (this->time_accumulated/this->shot_cooldown) - this->shots_fired;
+    if (shots_to_fire <= 0)
+        return;
+
+    double angle_step = 2 * Math_PI / bullets_per_shot;
+    for (int shot = 1; shot <= shots_to_fire; shot++)
+    {
+        for (int i = 0; i < bullets_per_shot; ++i)
+        {
+            double angle = i * angle_step;
+            godot::Vector3 dir = this->direction.rotated(VECTOR_UP, angle);
+
+            Projectile* projectile = Object::cast_to<Projectile>(projectile->duplicate());
+            ERR_FAIL_NULL(projectile);
+            projectile->set_position(this->actor_source->get_position());
+            projectile->set_direction(dir);
+            get_tree()->get_current_scene()->add_child(projectile);
+        }
+    } 
 }
 
 void CircleWaveAttack::_bind_methods()
@@ -11,4 +45,5 @@ void CircleWaveAttack::_bind_methods()
     BIND_GETTER_SETTER_PROPERTY_DEFAULT(CircleWaveAttack, Variant::INT, bullets_per_shot);
     BIND_GETTER_SETTER_PROPERTY_DEFAULT(CircleWaveAttack, Variant::INT, shots);
     BIND_GETTER_SETTER_PROPERTY_DEFAULT(CircleWaveAttack, Variant::FLOAT, shot_cooldown);
+    BIND_GETTER_SETTER_PROPERTY_DEFAULT(CircleWaveAttack, Variant::VECTOR2, direction);
 }
--- src/attacks/circle_wave_attack.hpp
+++ src/attacks/circle_wave_attack.hpp
@@ -12,18 +12,25 @@ private:
     Projectile* projectile; 
     double shot_cooldown = 1;
     int shots = 0;
+    int shots_fired = 0;
     int bullets_per_shot = 0;
-    double direction = 0;
+    godot::Vector3 direction = {1, 0, 0};
 
 public:
     CREATE_GETTER_SETTER_DEFAULT(Projectile*, projectile);
     CREATE_GETTER_SETTER_POSITIVE_DEFAULT(double, shot_cooldown);
     CREATE_GETTER_SETTER_POSITIVE_DEFAULT(int, shots);
     CREATE_GETTER_SETTER_POSITIVE_DEFAULT(int, bullets_per_shot);
-    CREATE_GETTER_SETTER_POSITIVE_DEFAULT(double, direction);
+
+    void set_direction(const godot::Vector3 direction);
+    _FORCE_INLINE_ godot::Vector3 get_direction() const
+    {
+        return this->direction;
+    }
+
 
 protected:
-    /* virtual void _reset() override; */
+    virtual void _reset() override;
     virtual void _step(double delta) override;
 
 protected:
