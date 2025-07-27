3573e6a9dbcd20162716c2cd3cf3172491d231e9
Alexander Makedonski <alemak2002@gmail.com>
2025-07-27 12:09:01 +0300

    Small fix to WaveAttack.

--- src/attacks/wave_attack.cpp
+++ src/attacks/wave_attack.cpp
@@ -15,7 +15,7 @@ void WaveAttack::_reset()
 
 void WaveAttack::_bind_methods()
 {
-    BIND_GETTER_SETTER_PROPERTY_OBJECT_DEFAULT(WaveAttack, projectile, PropertyHint::PROPERTY_HINT_NODE_TYPE, "Projectile", PropertyUsageFlags::PROPERTY_USAGE_DEFAULT, Projectile);
+    BIND_GETTER_SETTER_PROPERTY_OBJECT_DEFAULT(WaveAttack, projectile, PROPERTY_HINT_NODE_TYPE, "Projectile", PROPERTY_USAGE_DEFAULT, Projectile);
     BIND_GETTER_SETTER_PROPERTY_DEFAULT(WaveAttack, INT, projectiles_per_wave);
     BIND_GETTER_SETTER_PROPERTY_DEFAULT(WaveAttack, INT, waves);
     BIND_GETTER_SETTER_PROPERTY_DEFAULT(WaveAttack, FLOAT, wave_cooldown);
