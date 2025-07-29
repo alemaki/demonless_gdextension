05eddea7b48750ef4aa8e336e7f95d5e8fed6414
Alexander Makedonski <alemak2002@gmail.com>
2025-07-29 12:57:13 +0300

    Added tests for movement context.

---
+++ src/tests/unit/movement_strategies/test_movement_context.cpp
@@ -0,0 +1,47 @@
+#define DOCTEST_CONFIG_NO_EXCEPTIONS_BUT_WITH_ALL_ASSERTS
+#include "utils/utils.hpp"
+#include "movement_strategies/movement_context.hpp"
+
+struct MovementContextFixture {
+    MovementContext* context = nullptr;
+    godot::Node3D* dummy_target = nullptr;
+
+    MovementContextFixture() {
+        context = memnew(MovementContext);
+        dummy_target = memnew(godot::Node3D);
+    }
+
+    ~MovementContextFixture() {
+        memdelete(context);
+        memdelete(dummy_target);
+    }
+};
+
+TEST_SUITE("TestMovementContext")
+{
+    TEST_CASE_FIXTURE(MovementContextFixture, "Default values")
+    {
+        CHECK_VECTORS_NE(context->get_direction(), godot::Vector3(0, 0, 0));
+        CHECK_GE(context->get_speed(), doctest::Approx(0.0));
+        CHECK_EQ(context->get_target(), static_cast<godot::Node3D*>(nullptr));
+    }
+
+    TEST_CASE_FIXTURE(MovementContextFixture, "Setters and getters function properly")
+    {
+        context->set_position(godot::Vector3(2, 4, 6));
+        context->set_direction(godot::Vector3(-1, 0, 1));
+        context->set_speed(2.718);
+        context->set_target(dummy_target);
+
+        CHECK_EQ(context->get_position(), godot::Vector3(2, 4, 6));
+        CHECK_EQ(context->get_direction(), godot::Vector3(-1, 0, 1));
+        CHECK_EQ(context->get_speed(), doctest::Approx(2.718));
+        CHECK_EQ(context->get_target(), dummy_target);
+    }
+
+    TEST_CASE_FIXTURE(MovementContextFixture, "Speed does not accept negative values")
+    {
+        context->set_speed(-10.0);
+        CHECK_GE(context->get_speed(), doctest::Approx(0.0));
+    }
+}
