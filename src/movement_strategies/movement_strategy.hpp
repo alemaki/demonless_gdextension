51eb14b0234c4f35c58ef7028eb0ac7e7223d03b
Alexander Makedonski <alemak2002@gmail.com>
2025-09-25 23:42:01 +0300

    Added documentation to MovementStrategy.

--- src/movement_strategies/movement_strategy.hpp
+++ src/movement_strategies/movement_strategy.hpp
@@ -9,14 +9,33 @@ class MovementStrategy : public godot::Node
     GDCLASS(MovementStrategy, godot::Node);
 
 private:
+    /**
+     * @brief Applies the movement strategy to the context based on delta time variable.
+     *
+     * @param context reference to the movement context to apply the strategy to.
+     * @param delta the time variable for the movement strategy.
+     */
     virtual void _apply(godot::Ref<MovementContext> context, double delta);
 
 public:
+    /**
+     * @brief Checks if the strategy is done if yes - applies the default linear behaviour implemented by
+     * MovementStrategy to context based on delta time variable, if not - applies _apply() of the strategy.
+     *
+     * @param context reference to the movement context to apply the strategy to.
+     * @param delta the time variable for the movement strategy.
+     */
     void apply(godot::Ref<MovementContext> context, double delta);
+    /**
+     * @brief Checks if the movement strategy is finished with executing itself.
+     * Child classes define different meanings of is_done.
+     * @return true if done
+     * @return false if not done
+     */
     virtual bool is_done() const;
 
 protected:
     static void _bind_methods();
 };
 
-#endif // MOVEMENT_STRATEGY_HPP
+#endif /* MOVEMENT_STRATEGY_HPP */
